b110004f469cfaaa6a84666563f64780a6a57b1f
Mobb autofixer <git@mobb.ai>
2023-08-24 16:14:57 +0000

    mobb fix commit: feb1421b-a4a8-4310-ae0e-6bcb653b4884

--- juice-shop-master/routes/login.ts
+++ juice-shop-master/routes/login.ts
@@ -33,7 +33,9 @@ module.exports = function login () {
 
   return (req: Request, res: Response, next: NextFunction) => {
     verifyPreLoginChallenges(req) // vuln-code-snippet hide-line
-    models.sequelize.query(`SELECT * FROM Users WHERE email = '${req.body.email || ''}' AND password = '${security.hash(req.body.password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
+    const email = utils.sanitize(req.body.email) /*n8*/
+    const password = utils.sanitize(req.body.password) /*n9*/
+    models.sequelize.query(`SELECT * FROM Users WHERE email = '${email || ''}' AND password = '${security.hash(password || '')}' AND deletedAt IS NULL`, { model: UserModel, plain: true }) // vuln-code-snippet vuln-line loginAdminChallenge loginBenderChallenge loginJimChallenge
       .then((authenticatedUser: { data: User }) => { // vuln-code-snippet neutral-line loginAdminChallenge loginBenderChallenge loginJimChallenge
         const user = utils.queryResultToJson(authenticatedUser)
         if (user.data?.id && user.data.totpSecret !== '') {
